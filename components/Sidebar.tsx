9182b466e258e136b316b15bf6045d256e6637c9
weacond <weacond@gmail.com>
2026-03-10 13:59:01 +0800

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,14 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function Sidebar() {
   return (
-    <aside className="w-48 bg-gray-100 p-4 flex flex-col gap-2">
-      <Link href="/library" className="hover:bg-gray-200 p-2 rounded">我的业务</Link>
-      <Link href="/library" className="hover:bg-gray-200 p-2 rounded">电子书库</Link>
+    <aside className="w-48 bg-slate-800 min-h-screen p-4 flex flex-col gap-2">
+      <Link href="/library">
+        <a className="hover:bg-slate-700 px-2 py-1 rounded">电子书库</a>
+      </Link>
+      <Link href="/library">
+        <a className="hover:bg-slate-700 px-2 py-1 rounded">我的业务</a>
+      </Link>
     </aside>
-  );
+  )
 }
