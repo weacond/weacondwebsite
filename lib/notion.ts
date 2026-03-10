19d85b011dc075a29f2e45b7be52c4bcebbc2e83
weacond <weacond@gmail.com>
2026-03-10 14:02:28 +0800

    Update notion.ts

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,13 +1,5 @@
-import { Client } from "@notionhq/client";
+import { Client } from "@notionhq/client"
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
-
-export async function getDatabaseItems(databaseId: string) {
-  const response = await notion.databases.query({ database_id: databaseId });
-  return response.results;
-}
-
-export async function getPageContent(pageId: string) {
-  const blocks = await notion.blocks.children.list({ block_id: pageId });
-  return blocks.results;
-}
+export const notion = new Client({
+  auth: process.env.NOTION_API_KEY
+})
