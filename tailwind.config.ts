b821113474710ed92b58d3b86d847a3313f4fb9d
weacond <weacond@gmail.com>
2026-03-11 11:24:35 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
-export default config
+export default config;
