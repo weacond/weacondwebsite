b55c6e3074ac0954fe12fa94eac021dbf837f133
weacond <weacond@gmail.com>
2026-03-11 17:21:34 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
+import { LanguageProvider } from '../contexts/LanguageContext'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <LanguageProvider>
+      <Component {...pageProps} />
+    </LanguageProvider>
+  )
 }
