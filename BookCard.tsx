d2b4e072632d3afe168e4d53e513017f2dd0ba29
weacond <weacond@gmail.com>
2026-03-11 16:18:46 +0800

    Delete BookCard.tsx

--- BookCard.tsx
+++
@@ -1,8 +0,0 @@
-export default function BookCard({ book }) {
-  return (
-    <a href={`/books/${book.id}`} className="block border p-2 rounded hover:shadow">
-      <img src={book.cover} alt={book.title} className="w-full h-40 object-cover"/>
-      <p className="mt-2 text-center">{book.title}</p>
-    </a>
-  );
-}
