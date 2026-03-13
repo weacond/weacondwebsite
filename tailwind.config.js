6e4edbbba6293dda45cf2d9948b2afccd4f37f42
weacond <weacond@gmail.com>
2026-03-13 15:15:49 +0800

    Update and rename tailwind.config.ts to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./contexts/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
